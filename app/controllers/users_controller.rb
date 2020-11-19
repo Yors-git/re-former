57cf640822d2d823e26540bce8b1ddcd1f742c92
Gilbert Gotora <gilbertgotora2@gmail.com>
2020-11-19 17:50:39 +0200

    test form using different input

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     @user = User.new(username: params[:username], email: params[:email], password: params[:password])
   
     if @user.save
-      redirect_to new_user_path
+      redirect_to @user
     else
       render :new
     end
