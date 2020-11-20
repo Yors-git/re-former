e00ef34bd45f5ea472ec06de77b9558806bda22b
Yors <jatbribiesca@gmail.com>
2020-11-20 08:55:56 -0600

    fix linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
@@ -20,13 +20,13 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    
+
     render :edit
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:id,:username,:email,:password)
+    params.require(:user).permit(:id, :username, :email, :password)
   end
 end
