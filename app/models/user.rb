e3f2511e70ee659b68cea021a73f7108586a3131
Yors <jatbribiesca@gmail.com>
2020-11-20 19:41:08 -0600

    fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 3 }
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
-    message: "please enter a valid email" }
+                                              message: 'please enter a valid email' }
   validates :password, presence: true
 end
